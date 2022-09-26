c07de3bd932b565f77ea01c55eba96949325652a
Rickard Armiento <rickard-gpg@armiento.net>
2022-09-26 09:54:55 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,4 +3,4 @@
 HERE="${BASH_SOURCE[0]}"
 eval HEREDIR="$(dirname "$HERE")"
 DIR="$(cd "$HEREDIR"; pwd -P)"
-export PYTHONPATH="/courses/TFYA74/python:$DIR/dscribe:$PYTHONPATH"
+export PYTHONPATH="$PYTHONPATH:$DIR/dscribe"
